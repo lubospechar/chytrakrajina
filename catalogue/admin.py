366eb12832d66d7cbbe2e9cd015b2cd35107e3f8
Luboš Pechar <lubos.pechar@prirodou.cz>
2026-02-12 11:34:04 +0100

    Register `MeasureGroup` and `Measure` models in Django admin with customized configuration

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from catalogue.models import (
+    MeasureGroup,
+    Measure
+)
 
-# Register your models here.
+@admin.register(MeasureGroup)
+class MeasureGroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+
+@admin.register(Measure)
+class MeasureAdmin(admin.ModelAdmin):
+    list_display = ("name", "short_description")
+    list_filter = ("group",)
+    search_fields = ("name", "short_description")
+    ordering = ("name",)
+    fieldsets = (
+        (_("Base information"), {"fields": ("name", "short_description", "group")}),
+        (_("Scores"), {"fields": ("aesthetics", "air", "biodiversity", "temperature", "water")}),
+    )
+
