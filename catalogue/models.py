131c7a3510d8cb0209faa647c9ab3d2d41441420
Luboš Pechar <lubos.pechar@prirodou.cz>
2026-02-12 11:01:16 +0100

    Initialize `catalogue` app and configure in `INSTALLED_APPS`

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class MeasureGroup(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_("Measure group name"))
+
+    description = models.TextField(
+        verbose_name=_("Measure group description"))
+
+    icon = models.ImageField(
+        upload_to="measure_groups",
+        verbose_name=_("Measure group icon")
+    )
+
+
+class Measure(models.Model):
+    # base information
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_("Measure name"))
+
+    group = models.ForeignKey(
+        MeasureGroup,
+        on_delete=models.CASCADE,
+        verbose_name=_("Measure group")
+    )
+
+    short_description = models.CharField(
+        max_length=255,
+        verbose_name=_("Measure short description")
+    )
