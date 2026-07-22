e9341f6de9c07b8bd5719512c5a26b5a9d75d627
Luboš Pechar <lubos.pechar@prirodou.cz>
2026-07-22 10:51:41 +0200

    Rename `short_description` method to `short_name` in `MeasureGroup` model.

--- catalogue/models.py
+++ catalogue/models.py
@@ -86,11 +86,11 @@ class MeasureGroup(models.Model):
         else:
             return self.description_en
 
-    def short_description(self):
+    def short_name(self):
         if get_language() == "cs":
-            return self.short_description_cs
+            return self.short_name_cs
         else:
-            return self.short_description_en
+            return self.short_name_en
 
     def slug(self):
         if get_language() == "cs":
